2f8f663716b2511a6c7b9e39d52bacc1bfd64487
Sena Nur Bakır <150732354+ssenanb@users.noreply.github.com>
2026-08-25 18:45:34 +0300

    Update subscriber-throughput.c

--- dds_workspace4/subscriber-throughput.c
+++ dds_workspace4/subscriber-throughput.c
@@ -7,7 +7,6 @@
 
 #define MAX_SAMPLES 1
 
-
 int64_t get_time_us(void)
 {
     struct timespec ts;
@@ -23,35 +22,28 @@ int main(int argc, char **argv)
     dds_entity_t topic;
     dds_entity_t reader;
     dds_return_t rc;
-    dds_qos_t *qos;
     myMessage_Msg *msg;
     uint64_t counter = 0;
     uint64_t pkg_counter = 0;
     uint64_t startTime;
-    void *samples[MAX_SAMPLES]; // the free space created in RAM with myMessage_Msg__alloc()
-    dds_sample_info_t infos[MAX_SAMPLES]; // for status control such as valid
+    void *samples[MAX_SAMPLES]; 
+    dds_sample_info_t infos[MAX_SAMPLES];
 
-    // Create a Participant
     participant = dds_create_participant (DDS_DOMAIN_DEFAULT, NULL, NULL);
     if (participant < 0)
         DDS_FATAL("dds_create_participant: %s\n", dds_strretcode(-participant));
-
-    //qos = dds_create_qos();
-    //dds_qset_reliability(qos, DDS_RELIABILITY_BEST_EFFORT, 0);
-
 
     topic = dds_create_topic (participant, &myMessage_Msg_desc, "myMessage_Msg_", NULL, NULL);
     if (topic < 0)
             DDS_FATAL("dds_create_topic: %s\n", dds_strretcode(-topic));
 
-    reader = dds_create_reader (participant, topic, NULL, NULL); //QoS
+    reader = dds_create_reader (participant, topic, NULL, NULL); 
     if (reader < 0)
             DDS_FATAL("dds_create_reader: %s\n", dds_strretcode(-reader));
 
     printf("---SUBSCRIBER, TOPIC AND READER ARE CREATED---\n");
     fflush (stdout);
     
-    // Allocate space equal to the message size and samples[0] is starting point
     samples[0] = myMessage_Msg__alloc ();
 
     while(1)
@@ -96,7 +88,6 @@ int main(int argc, char **argv)
         }
     }
 
-    // Release the resources
     myMessage_Msg_free (samples[0], DDS_FREE_ALL);
 
     rc = dds_delete (participant);
