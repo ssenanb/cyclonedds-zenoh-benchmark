a0b63598d1089ee345499ae2d36322c9353a3917
Sena Nur Bakır <150732354+ssenanb@users.noreply.github.com>
2026-08-25 18:55:44 +0300

    Update subscriber-throughput.c

--- zp_workspace1/subscriber-throughput.c
+++ zp_workspace1/subscriber-throughput.c
@@ -57,6 +57,7 @@ int main (int argc, char** argv)
     z_owned_config_t config; 
     z_config_default(&config);
 
+    // UDP Multicast - p2p
     zp_config_insert(z_loan_mut(config), Z_CONFIG_MODE_KEY, Z_CONFIG_MODE_PEER);
     zp_config_insert(z_loan_mut(config), Z_CONFIG_LISTEN_KEY, "udp/224.0.0.123:7447#iface=lo");
    
